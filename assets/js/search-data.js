470424503263a66935530d25993d68af1e2f35a0
LennyTaelman <145012882+LennyTaelman@users.noreply.github.com>
2025-01-14 10:08:04 +0000

    Deploying to gh-pages from @ LennyTaelman/LennyTaelman.github.io@b7c1274351999ba910c719cec73deddaa1db839f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -346,16 +346,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-wouter-rienks-has-defended-his-thesis-deforming-fourier-mukai-transforms-congratulations-to-wouter",
+          title: 'Wouter Rienks has defended his thesis Deforming Fourier-Mukai transforms. Congratulations to Wouter!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
